c4e240d7d06bfc69585714aae244c25a90e9ce71
Nikhil Vijayan <nikhilvijayan2004@gmail.com>
2026-01-29 10:58:47 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name
+{
+    public static void main(String args[])
+    {
+        int age=10;
+        String name="Binu";
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+}
